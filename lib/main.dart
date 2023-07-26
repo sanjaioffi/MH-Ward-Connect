602922d95ef809bca3edef1cc57bb09b605bf64f
Mitun Suresh <mitun.focused@gmail.com>
2023-07-26 10:08:01 +0530

    clone checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:madurai_ward_connect/src/presentation/screens/mainpage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
